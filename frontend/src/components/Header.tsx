c0274e912399bb84dd09598f73900fe478f6b079
Kaloyan Drenski <drenski666@gmail.com>
2022-03-12 22:30:19 +0200

    header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -20,20 +20,10 @@ const Header = (props: Props) => {
               navbarScroll
             >
               <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-              <NavDropdown title="Link" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
-                  Something else here
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#" disabled>
-                Link
-              </Nav.Link>
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action1">Home</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
