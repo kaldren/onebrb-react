d8f688f5db955b213a9704cbba6f520912cc8fea
Kaloyan Drenski <drenski666@gmail.com>
2022-03-13 09:43:15 +0200

    Test profiles data

---
+++ frontend/profiles.js
@@ -0,0 +1,32 @@
+const profiles = [
+    {
+        id: '1',
+        firstName: 'John',
+        middleName: 'Johnson',
+        familyName: "Doe",
+        birthDate: '05/05/1993',
+        email: 'drenski666@gmail.com',
+        phone: '+359888778877',
+        description: '.NET Developer for the past 4 years.'
+    },
+    {
+        id: '2',
+        firstName: 'Bob',
+        middleName: 'Johnson',
+        familyName: "Harris",
+        birthDate: '05/05/1981',
+        email: 'bob@gmail.com',
+        phone: '+359888223344',
+        description: 'Java Developer for the past 14 years.'
+    },
+    {
+        id: '3',
+        firstName: 'Steve',
+        middleName: 'Dick',
+        familyName: "Ballmer",
+        birthDate: '05/05/1959',
+        email: 'ballmer@gmail.com',
+        phone: '+359888111111',
+        description: 'Boss.'
+    },
+]
