5d1f078680341214691c63bedbb31bb9e2f10b09
Kaloyan Drenski <drenski666@gmail.com>
2022-03-18 23:28:14 +0200

    Refactor profile

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDb from './config/db.js';
+import profiles from "./data/profiles.js";
 dotenv.config();
 connectDb();
 const app = express();
@@ -8,7 +9,7 @@ app.get('/', (req, res) => {
     res.send('Hi!');
 });
 app.get('/api/profiles', (req, res) => {
-    res.json({});
+    res.json(profiles);
 });
 // app.get('/api/profiles/:id', (req, res) => {
 //     const profile = profiles.find(p => p.id === req.params.id);
--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDb from './config/db.js';
+import profiles from "./data/profiles.js";
 
 dotenv.config()
 
@@ -13,7 +14,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/profiles', (req, res) => {
-    res.json({});
+    res.json(profiles);
 })
 
 // app.get('/api/profiles/:id', (req, res) => {
